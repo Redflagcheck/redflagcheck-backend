72f089a62df122fc9648775c37c5f02e2df16dcf
Dateformula <dateformula@gmail.com>
2025-08-06 10:26:33 +0200

    Update views.py

--- redflagcheck/views.py
+++ redflagcheck/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .models import User, Analysis
+from rest_framework.parsers import MultiPartParser, FormParser
+
 
 # Create your views here.
 
@@ -16,20 +18,29 @@ def home(request):
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def form_submit(request):
-    # 1. Gegevens uit POST-body halen
+    # Ondersteunt nu ook file uploads!
     email = request.data.get('email')
     name = request.data.get('name', '')
     message = request.data.get('message')
     context = request.data.get('context', '')
-    screenshot_url = request.data.get('screenshot_url', '')
+
+    # Bestand ophalen uit FILES (optioneel)
+    screenshot_file = request.FILES.get('screenshot')
+    screenshot_url = None
+
+    # Bestand opslaan als het bestaat
+    if screenshot_file:
+        from django.core.files.storage import default_storage
+        path = default_storage.save('uploads/' + screenshot_file.name, screenshot_file)
+        screenshot_url = default_storage.url(path)
+    else:
+        screenshot_url = request.data.get('screenshot_url', '')
 
     if not email or not message:
         return Response({'status': 'error', 'message': 'Email en bericht zijn verplicht.'}, status=400)
 
-    # 2. User ophalen of aanmaken
     user, _ = User.objects.get_or_create(email=email, defaults={'name': name})
 
-    # 3. Analysis opslaan
     analysis = Analysis.objects.create(
         user=user,
         message=message,
