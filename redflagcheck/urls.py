172cce2c15a802e3757c7f829d26ebe233a9352e
Dateformula <dateformula@gmail.com>
2025-08-06 10:11:29 +0200

    x
    d

--- redflagcheck/urls.py
+++ redflagcheck/urls.py
@@ -1,10 +1,6 @@
-# redflagcheck/urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('save_formdata', views.form_submit, name='save_formdata'),  # ✅ correcte koppeling
-    # eventueel later:
-    # path('email_status', views.email_status, name='email_status')
+    path('save_formdata/', views.form_submit, name='save_formdata'),
 ]
