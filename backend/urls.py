6415d47b4841ae25d7947f998ec52abdcc6e7d3d
Dateformula <dateformula@gmail.com>
2025-08-07 19:07:24 +0200

    d
    d

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from redflagcheck import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/verify_token/', views.verify_token),  # ← forceer directe toegang
     path('api/', include('redflagcheck.urls')),
 ]
 
