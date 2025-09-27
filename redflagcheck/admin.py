68fd4754db08ea86196ba6731a3427bbb383b944
Dateformula <dateformula@gmail.com>
2025-09-27 15:38:53 +0200

    Update admin.py

--- redflagcheck/admin.py
+++ redflagcheck/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from .models import Analysis, Followup, AuditEvent
+from .models import Analysis
 
 @admin.register(Analysis)
 class AnalysisAdmin(admin.ModelAdmin):
-    list_display = ("analysis_id", "email", "status", "created_at", "completed_at")
-    readonly_fields = ("created_at", "updated_at", "completed_at")
-    search_fields = ("analysis_id", "email", "input_text")
-    list_filter = ("status", "created_at", "completed_at")
-    ordering = ("-created_at",)
+    pass
